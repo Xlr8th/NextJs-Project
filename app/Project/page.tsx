1bc098161a29eb2d739ad3490fd0772202d3d005
Xlr8th <saintgeorge.001@gmail.com>
2025-12-02 11:29:32 +0100

    adjustment

---
+++ app/Project/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const project = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is my first custom page in NextJS.</p>
+      <p>This is my first custom page in NextJS.</p>
+      <p>This is my first custom page in NextJS.</p>
+    </div>
+  )
+}
+
+export default project
